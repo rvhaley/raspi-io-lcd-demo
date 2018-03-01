ea2143fb5b49f6baaf88891b58143b11d0651d84
Richard Haley III <rvhaley@hotmail.com>
2018-02-28 20:10:23 -0800

    Added lcd object with raspi-io pins.

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const board = new five.Board({
 });
 
 board.on("ready", () => {
-	(new five.Led("GPIO5")).strobe();
+	var lcd = new five.LCD({
+		pins: ["GPIO5", "GPIO6", "GPIO13", "GPIO16", "GPIO19", "GPIO20"],
+		rows: 2,
+		cols: 16
+	});
+
+	lcd.clear().print("Hello, World!");
 });
 
