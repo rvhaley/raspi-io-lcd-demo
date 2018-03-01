910b0e84ca26ab5b0e32391d7e8b7856931f2144
Richard Haley III <rvhaley@hotmail.com>
2018-02-28 19:13:31 -0800

    Initial import into repository.

---
+++ app.js
@@ -0,0 +1,11 @@
+const Raspi = require("raspi-io");
+const five = require("johnny-five");
+
+const board = new five.Board({
+	io : new Raspi();
+});
+
+board.on("ready", () => {
+	(new five.Led("GPIO5")).strobe();
+});
+
